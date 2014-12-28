fbdedd449836c358bd058e7c94a2c14bf5f7938d
Hyeon Kim <simnalamburt@gmail.com>
2014-12-28 14:33:28 +0900

    2차 수정
    알고리즘 손 안대고 코딩스타일만 수정

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <string>
 #include <vector>
 #include <iostream>
@@ -6,10 +5,7 @@
 
 using namespace std;
 
-pair<string, string> pair1;
 vector<pair<string, string>> whitebook;
-string temp;
-string temp1;
 
 void search();
 void AddPhone();
@@ -17,14 +13,14 @@ void Delete();
 
 int main() {
   while (true) {
-    puts("무엇을 하시겠습니까\n"
+    cout << "무엇을 하시겠습니까\n"
       "1. Search\n"
       "2. Add\n"
       "3. Delete\n"
-      "4. Finish");
+      "4. Finish\n" << flush;
 
     int a;
-    scanf("%d", &a);
+    cin >> a;
 
     switch (a) {
     case 1:
@@ -48,56 +44,84 @@ int main() {
 
 
 void search() {
-  int i, save, count=0;
-  printf("이름을 입력하시오");
-  cin >> temp;
-  for(i=0; i< whitebook.size(); i++){
-    if(whitebook[i].first.compare(temp)==0){
-      count=count+1;
-      save=i;
-    }
+  cout << "이름을 입력하시오: " << flush;
+
+  string input;
+  cin >> input;
+
+  size_t save, count = 0;
+  const size_t len = whitebook.size();
+  for (size_t i = 0; i < len; ++i) {
+    if (whitebook[i].first.compare(input)) { continue; }
+
+    ++count;
+    save = i;
   }
-  if(count==0) printf("해당 이름의 전화번호는 존재하지 않습니다.");
-  else if(count==1){
-    cout<<whitebook[save].second<< "\n";
+
+  switch (count) {
+    case 0:
+      cout << "해당 이름의 전화번호는 존재하지 않습니다." << endl;
+      break;
+    case 1:
+      cout << whitebook[save].second << endl;
+      break;
+    default:
+      cout << "error: 해당 이름에 2개 이상의 전화번호가 저장되어 있습니다." << endl;
+      break;
   }
-  else printf("error: 해당 이름에 2개 이상의 전화번호가 저장되어 있습니다.");
 }
 
 void AddPhone() {
-  int i, save,count=0;
-  printf("이름을 입력하시오");
-  cin >> temp;
-  for(i=0; i< whitebook.size(); i++){
-    if(whitebook[i].first.compare(temp)==0){
-      count=count+1;
-      save=i;
-    }
+  cout << "이름을 입력하시오: " << flush;
+
+  string input;
+  cin >> input;
+
+  size_t save, count = 0;
+  const size_t len = whitebook.size();
+  for(size_t i = 0; i < len; ++i) {
+    if (whitebook[i].first.compare(input)) { continue; }
+
+    ++count;
+    save = i;
   }
-  if(count!=0) printf("해당 이름의 전화번호는 이미 저장되어있습니다.");
-  else{
-    printf("해당 이름의 전화번호를 입력해주십시오.");
-    cin >> temp1;
-    pair1.first=temp;
-    pair1.second=temp1;
+
+  if (count) {
+    cout << "해당 이름의 전화번호는 이미 저장되어있습니다." << endl;
+  } else {
+    cout << "해당 이름의 전화번호를 입력해주십시오: " << endl;
+
+    string number;
+    cin >> number;
+
+    pair<string, string> pair1;
+    pair1.first = input;
+    pair1.second = number;
     whitebook.push_back(pair1);
-    printf("저장이 완료되었습니다.");
+
+    cout << "저장이 완료되었습니다." << endl;
   }
 }
 
 void Delete() {
-  int i, save,count=0;
-  printf("이름을 입력하시오");
-  cin >> temp;
-  for(i=0; i< whitebook.size(); i++){
-    if(whitebook[i].first.compare(temp)==0){
-      count=count+1;
-      save=i;
-    }
+  cout << "이름을 입력하시오: " << flush;
+
+  string input;
+  cin >> input;
+
+  size_t save, count = 0;
+  const size_t len = whitebook.size();
+  for(size_t i = 0; i < len; ++i) {
+    if (whitebook[i].first.compare(input)) { continue; }
+
+    ++count;
+    save = i;
   }
-  if(count==0) printf("해당 이름의 전화번호는 존재하지 않습니다.");
-  if(count==1){
-    whitebook.erase(whitebook.begin()+save);
-    printf("삭제가 완료되었습니다.");
+
+  if (!count) {
+    cout << "해당 이름의 전화번호는 존재하지 않습니다." << endl;
+  } else if (count == 1) {
+    whitebook.erase(whitebook.begin() + save);
+    cout << "삭제가 완료되었습니다." << endl;
   }
 }
