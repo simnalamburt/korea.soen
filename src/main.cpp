434df31ff564424b2a96121c8effa71dbf182c91
Hyeon Kim <simnalamburt@gmail.com>
2014-12-28 23:09:14 +0900

    프로그램 끝날때 엔터 한번치고 끝나도록

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,10 @@ int main() {
     if (a == "1")       { search(); }
     else if (a == "2")  { AddPhone(); }
     else if (a == "3")  { Delete(); }
-    else if (a == "4")  { return 0; }
+    else if (a == "4")  { break; }
     else { cout << "유효한 입력이 아닙니다." << endl; }
   }
 
+  cout << endl;
   return 0;
 }
